a672a428ce0aa3bcf0185cb22ae39785a693d756
fred <popvlous@gmail.com>
2021-04-21 11:51:01 +0800

    fix db

--- app/__init__.py
+++ app/__init__.py
@@ -7,15 +7,9 @@ from flask import Flask, url_for
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from importlib import import_module
-from flask_sqlalchemy import SQLAlchemy as _BaseSQLAlchemy
 
 from logging import basicConfig, DEBUG, getLogger, StreamHandler
 from os import path
-
-class SQLAlchemy(_BaseSQLAlchemy):
-    def apply_pool_defaults(self, app, options):
-        super(SQLAlchemy, self).apply_pool_defaults(app, options)
-        options['pool_pre_ping'] = True
 
 db = SQLAlchemy()
 login_manager: LoginManager = LoginManager()
