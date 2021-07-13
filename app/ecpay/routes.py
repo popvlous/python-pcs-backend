139e46d7270ed48b6eaed005264841733a9aeca0
fred <popvlous@gmail.com>
2021-07-13 16:35:58 +0800

    1.fix invoice_info

--- app/ecpay/routes.py
+++ app/ecpay/routes.py
@@ -265,7 +265,7 @@ def payment_info():
         'RelateNumber': str(order_id) + datetime.now().strftime("IN%Y%m%d%H%M%S"),  # 特店自訂編號
         # 'RelateNumber': str(order_id),  # 特店自訂編號
         'CustomerID': str(order_details['customer_id']),  # 客戶編號
-        'CustomerIdentifier': str(customer_identifier),  # 統一編號
+        'CustomerIdentifier': customer_identifier,  # 統一編號
         'CustomerName': invoice_name,
         'CustomerAddr': invoice_address,
         'CustomerPhone': billing['phone'],  # 客戶手機號碼
